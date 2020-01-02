4545c66ccb89497e750e91b2615bf888a98f292c
Aman941 <aman.verma9412345@gmail.com>
2020-01-03 02:27:45 +0530

    second commit

---
+++ precache-manifest.bf1d0768128ce61d66c8fc5f6af6becd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bb00d10b896f93231e65238e2ce3a66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f69e56221882cf711197",
+    "url": "/static/css/main.3a1c619b.chunk.css"
+  },
+  {
+    "revision": "827853150f6a3f536db5",
+    "url": "/static/js/2.1de4ab55.chunk.js"
+  },
+  {
+    "revision": "2931b869d291f9f0087a1920cf96ae61",
+    "url": "/static/js/2.1de4ab55.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f69e56221882cf711197",
+    "url": "/static/js/main.a56c3047.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  }
+]);
